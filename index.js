1a53db6dd1c0dcd064580130260427ba3ecfb28d
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-12 17:43:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(Math.random() * 10)
+
+const mod = 14 % 5
+
+const max = Math.max(16, 17, 18, 19, 20)
+tw3DenYXA2q4SNK
